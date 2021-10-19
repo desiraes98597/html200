e9f1979c598b82ec7b23f8311b08e55e8848c11e
desiraes98597 <86029490+desiraes98597@users.noreply.github.com>
2021-10-18 18:06:25 -0700

    Add files via upload

---
+++ lesson02/calculator.js
@@ -0,0 +1,11 @@
+const x = 1;
+const y = 2;
+const z = 3;
+const w = 4;
+
+console.log(x - y);               
+console.log(z * (y -w));
+console.log(z / w);
+console.log(x + z -(w * y));
+console.log(x + y + 3);
+console.log(w / 25);
